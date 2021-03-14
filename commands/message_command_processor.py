e535e8e6dc46972e3b0a86e52dadbdc1ace6395a
Carlo <carlohenrique78@gmail.com>
2021-03-14 15:25:33 -0300

    Add .env-example

--- commands/message_command_processor.py
+++ commands/message_command_processor.py
@@ -1,4 +1,7 @@
-
+# This module will display the message 
 
 async def message_command_processor(ctx, data):
+
+    # precisa validar se a pessoa tem permissão para executar o comando
+
     await ctx.channel.send(data["data"])
